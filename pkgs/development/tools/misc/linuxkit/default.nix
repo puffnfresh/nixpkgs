144b5f3178e97dfba637c9c855bca6dc04113b3f
Brian McKenna <brian@brianmckenna.org>
2018-02-21 23:20:13 -0800

    update linuxkit

--- pkgs/development/tools/misc/linuxkit/default.nix
+++ pkgs/development/tools/misc/linuxkit/default.nix
@@ -2,22 +2,21 @@
 
 buildGoPackage rec {
   name = "linuxkit-${version}";
-  version = "2017-09-08";
-  rev = "c793ea973cdfb98888d9d972d567cb988936263a";
+  version = "0.2";
 
   goPackagePath = "github.com/linuxkit/linuxkit";
 
   src = fetchFromGitHub {
     owner = "linuxkit";
     repo = "linuxkit";
-    inherit rev;
-    sha256 = "1bncj7hbwhmsl53qdh0zhkxrr4irchh92nh5i9ydisp23qzfkzh1";
+    rev = "v${version}";
+    sha256 = "1y7pjmzimnm52v218fznqg8gjiwzxg38ywxiqig8iiljpc6hiyha";
   };
 
   subPackages = [ "src/cmd/linuxkit" ];
 
   preBuild = ''
-    buildFlagsArray+=("-ldflags" "-X main.GitCommit=${builtins.substring 0 7 rev} -X main.Version=0.0.0")
+    buildFlagsArray+=("-ldflags" "-X main.GitCommit=1c552f7 -X main.Version=0.2.0")
   '';
 
   meta = {
